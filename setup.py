1a99c85a24f01e2889cd881ecbcc393114549449
Jay Ravaliya <jayrav13@gmail.com>
2016-08-25 16:12:02 -0400

    Updating setup.py to v0.2.

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from distutils.core import setup
 setup(
     name = 'ical_dict',
     packages = ['ical_dict'],
-    version = '0.1',
+    version = '0.2',
     description = 'A Python library to convert an .ics file into a Dictionary object.',
     author = 'Jay Ravaliya',
     author_email = 'jayrav13@gmail.com',
     url = 'https://github.com/jayrav13/ical_dict',
-    download_url = 'https://github.com/jayrav13/ical_dict/tarball/0.1',
+    download_url = 'https://github.com/jayrav13/ical_dict/tarball/0.2',
     keywords = ['calendar', 'ical', 'ics', 'json', 'dictionary', 'python'],
     classifiers = [],
 )
